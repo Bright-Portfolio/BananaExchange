8a9b2471d2d9d55c59cf9ed04eae1ed80eafe7d8
BrightNtt <big_brightstepup@windowslive.com>
2025-06-27 10:01:10 +0700

    add symbols func but not done yet

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const baseCur = document.querySelector("#base-cur");
 const targetCur = document.querySelector("#target-cur");
 const inputAmount = document.querySelector("#amount");
 const conBtn = document.querySelector("#con-btn");
+const conResult = document.querySelector("#con-result");
 // console.dir(exForm);
+
+// fetch symbols data for slection
+//this func is not finish yet the data not filter only the country code that is global.
+const symbols = async () => {
+  try {
+    const symbolsRes = await fetch(
+      `https://api.currencyfreaks.com/v2.0/supported-currencies`
+    );
+    const symbolsData = await symbolsRes.json();
+    console.log(symbolsData);
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+symbols();
 
 async function exRate() {
   const base = baseCur.value;
@@ -14,7 +30,10 @@ async function exRate() {
   const url = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${accesskey}&symbols=${target}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.rates);
+  const rate = data.rates;
+  console.log(rate);
+
+  conResult.innerHTML = data.rates.EUR;
 }
 
 formEle.addEventListener("submit", (e) => {
@@ -22,5 +41,8 @@ formEle.addEventListener("submit", (e) => {
   exRate();
 });
 
-// note:ลอง loop เอาสกุุลทั้งหมดที่ api นี้ support มาใส่ไว้ใน option 
+// note:ลอง loop เอาสกุุลทั้งหมดที่ api นี้ support มาใส่ไว้ใน option
 // หาวิธีว่าเราจะทำยังไงให้ การคำนวณสกุลตอบโจทย์ user มากที่สุดใน api แบบฟรีนี้
+//ดึงสกุลแต่ลัตัวออกมาโดย filter หาข้อมูลที่ country code != global
+//problems:
+//ยังคงมีปัยหาเลือกการดึงข้อมูลอัตราแลกเปลี่ยนเนื่องจาก ยังหาวิธีเข้าถึงโดยที่ไม่ต้องมานั่งพิมสกุลเงินตัวสุดท้ายของตัวแปรแบบอัตโนมัติไม่ได้
